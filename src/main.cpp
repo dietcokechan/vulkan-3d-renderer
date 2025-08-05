26996cc7f60193114b625b25d860d110f9c52352
dietcokechan <dietcokechan@gmail.com>
2025-08-05 19:52:58 +0300

    vulkan instance created

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vulkan/vulkan.h>
 
 int main() {
   if (!glfwInit()) {
@@ -16,10 +17,37 @@ int main() {
     return -1;
   }
   
+  VkInstance instance;
+
+  VkApplicationInfo appInfo{};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Hello Vulkan!";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "No Engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+
+  uint32_t glfwExtensionCount = 0;
+  const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+  createInfo.enabledExtensionCount = glfwExtensionCount;
+  createInfo.ppEnabledExtensionNames = glfwExtensions;
+  createInfo.enabledLayerCount = 0;
+
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to create Vulkan instance!");
+  } else {
+    std::cout << "Vulkan instance created successfully!" << std::endl;
+  }
+
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
   }
 
+  vkDestroyInstance(instance, nullptr);
   glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
